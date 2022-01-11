07a2ebdf244eb7757242dce09d57b851505f3ec0
titusefferian <kiritosato@gmail.com>
2022-01-11 18:40:34 +0700

    admin submit

--- components/Admin/index.tsx
+++ components/Admin/index.tsx
@@ -1,69 +1,13 @@
-import firebase from "../../firebase/client";
 import "firebase/auth";
-import { useEffect, useState } from "react";
-import { Button, Typography } from "antd";
 import AdminForm from "./AdminForm";
-import { ADMIN_ID } from "../../constants";
-
-const provider = new firebase.auth.GoogleAuthProvider();
+import useUser from "../../hooks/useUser";
 
 const Admin = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState({
-    uid: "",
-  });
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (!user) {
-        setIsLoggedIn(false);
-      } else {
-        setUser({
-          uid: user.uid,
-        });
-        setIsLoggedIn(true);
-      }
-    });
-  }, []);
-  if (isLoggedIn && ADMIN_ID.includes(user.uid)) {
-    return (
-      <>
-        <AdminForm />
-        <Button
-          onClick={() => {
-            firebase.auth().signOut();
-          }}
-        >
-          logout
-        </Button>
-      </>
-    );
+  const { isLoggedIn } = useUser();
+  if (isLoggedIn) {
+    return <AdminForm />;
   }
-  if (isLoggedIn && !ADMIN_ID.includes(user.uid)) {
-    return (
-      <>
-        <Typography.Title>forbidden</Typography.Title>
-        <Button
-          onClick={() => {
-            firebase.auth().signOut();
-          }}
-        >
-          logout
-        </Button>
-      </>
-    );
-  }
-  return (
-    <>
-      <Typography.Title>Forbidden</Typography.Title>
-      <Button
-        onClick={() => {
-          firebase.auth().signInWithPopup(provider);
-        }}
-      >
-        Login
-      </Button>
-    </>
-  );
+  return null;
 };
 
 export default Admin;
